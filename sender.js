3c94b802a4fb5f59bacf02639689014c12ceb2c6
define) <120991965+definem@users.noreply.github.com>
2023-06-13 21:28:57 +0500

    Our bot is half ready

--- sender.js
+++ sender.js
@@ -1,16 +1,43 @@
-const bot = new Bot(
-  "6049056801:AAFZlNjzOBamhA-r8IlMh2Q99PhEOQ7xSzA",
-  "5625397913"
-);
+// const bot = new Bot(
+//   "6049056801:AAFZlNjzOBamhA-r8IlMh2Q99PhEOQ7xSzA",
+//   "5625397913"
+// );
 
-const input = document.getElementById("input");
-const btn = document.getElementById("btn");
+// const input = document.getElementById("input");
+// const btn = document.getElementById("btn");
 
-btn.addEventListener("click", () => {
-  bot
-    .sendMessage(input.value, null, null, true)
-    .then((res) => {
-      console.log("Success!", bot.sendMessage("Shu userdan: @" + res.result.chat.username));
+// btn.addEventListener("click", () => {
+//   bot
+//     .sendMessage(input.value, null, null, true)
+//     .then((res) => {
+//       console.log("Success!", bot.sendMessage("Shu userdan: @" + res.result.chat.username));
+//     })
+//     .catch((err) => console.log(err));
+// });
+
+const form = document.querySelector("#form");
+
+let bot = {
+  TOKEN: "6049056801:AAFZlNjzOBamhA-r8IlMh2Q99PhEOQ7xSzA",
+  chatID: "-1001652497227",
+};
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  let message = document.querySelector("#input");
+
+  fetch(
+    `https://api.telegram.org/bot${bot.TOKEN}/sendMessage?chat_id=${bot.chatID}&text=${message.value}`,
+    {
+      method: "GET",
     })
-    .catch((err) => console.log(err));
-});
+    .then(success => {
+      alert("Message sent successfully!");
+    }, error => {
+      alert("Message failed to be sent");
+      console.log(error);
+    }
+    
+    )
+})
